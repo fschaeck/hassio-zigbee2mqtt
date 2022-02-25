7965dd88f94566b081d217efbd95462957734212
Frank Schäckermann <github-fschaeckermann@snkmail.com>
2022-02-25 08:42:27 +0100

    Adding option zigbee_inject_for_test

--- common/rootfs/etc/cont-init.d/zigbee2mqtt.sh
+++ common/rootfs/etc/cont-init.d/zigbee2mqtt.sh
@@ -45,6 +45,18 @@ else
     fi
 fi
 
+if bashio::config.true 'zigbee_inject_for_test'; then
+    bashio::log.debug "Searching for directory inject_for_test in zigbee2mqtt data path..."
+    if bashio::fs.directory_exists "$DATA_PATH/inject_for_test"; then
+        bashio::log.info "Directory inject_for_test found, copying to ./node_modules/"
+        bashio::log.info "$(cp -afv "$DATA_PATH"/inject_for_test/. ./node_modules/)"
+    else
+        bashio::log.warning "No inject_for_test directory found in data path, no overwrites copied."
+    fi
+else
+    bashio::log.debug "No inject_for_test directory manipulation required"
+fi
+ 
 if bashio::config.true 'zigbee_shepherd_devices'; then
     bashio::log.debug "Searching for custom devices.js file in zigbee2mqtt data path..."
     if bashio::fs.file_exists "$DATA_PATH/devices.js"; then
